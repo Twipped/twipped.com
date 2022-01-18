c9e073782829fb955ba45823dc771de539c65735
Jocelyn Badgley (Twipped) <joc@twipped.com>
2022-01-17 17:39:59 -0800

    Adding the lifespark demo to terraform

--- terra/main.tf
+++ terra/main.tf
@@ -103,6 +103,7 @@ resource "aws_route53_record" "dkim" {
 # -----------------------------------------------------------------------------------------------------------
 # SSL Certificate
 
+# aws_acm_certificate.cert
 resource "aws_acm_certificate" "cert" {
   domain_name       = var.domain
   validation_method = "DNS"
@@ -124,6 +125,7 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
+# aws_route53_record.cert_validation
 resource "aws_route53_record" "cert_validation" {
   for_each = {
     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
