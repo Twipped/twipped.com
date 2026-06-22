54d2397e35977042dd0252a41aa0731fc033e1ab
Jocelyn Badgley (Twipped) <joc@twipped.com>
2026-06-22 14:42:15 -0700

    Migrate from react to vanilla css

--- src/common/fills/null.js
+++
@@ -1,3 +0,0 @@
-
-const nothing = null;
-export default nothing;
--- src/common/fills/object-assign.cjs
+++
@@ -1,7 +0,0 @@
-
-/**
- * This is just here so we can eliminate the object-assign dependency from builds.
- */
-module.exports = exports = function objectAssign (...args) { // eslint-disable-line
-  return Object.assign(...args);
-};
